81cfca762c9239a1fa405f7ed189f3b85cadbfeb
David Cuevas <davidjfcpage@gmail.com>
2018-07-30 18:40:07 +0100

    added pre-defined functional interfaces. Very useful

---
+++ src/main/java/cc_javautilfunctionpackage/PredefinedFunctinalInterfacesMain.java
@@ -0,0 +1,26 @@
+package cc_javautilfunctionpackage;
+
+import java.util.function.IntFunction;
+import java.util.function.IntPredicate;
+import java.util.function.IntToDoubleFunction;
+
+public class PredefinedFunctinalInterfacesMain {
+
+
+    public static void main(String[] args) {
+
+        IntPredicate greaterThan20 = p -> p > 20;
+
+        System.out.println(greaterThan20.test(10));
+
+        IntFunction<String> stringifyInteger = p -> String.format("%d stringified", p);
+
+        System.out.println(stringifyInteger.apply(10));
+
+        IntToDoubleFunction doublifyFunction = p -> Double.valueOf(p);
+
+        System.out.println(String.format("%.2f doublyfied into ", doublifyFunction.applyAsDouble(10)));
+
+
+    }
+}
